1accfb6ec7673cebca29a53626152b9b82948b45
ayushvds <ayush@videsigns.uk>
2024-08-29 18:57:16 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,3 +1,12 @@
-console.log("Fanum tax to compensated by VDS!");
+// Function to load the npm-hosted script
+function loadNpmScript() {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/test-logic-vds@latest/logic.js';
+    script.type = 'text/javascript';
+    document.head.appendChild(script);
+}
 
-// testing how soon will the script get updated
+// Load the npm script when the GitHub script is executed
+loadNpmScript();
+
+// Optionally, you can add other code here that should execute after loading the npm script.
